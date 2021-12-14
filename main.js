d25534e1e893a04d350b61a6d21c7387228c24d9
JERVINFRANCISCO <FRAN0436@ALGONQUINLIVE.COM>
2021-12-14 12:05:14 -0500

    ds

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var myHeaders = new Headers();
 myHeaders.append("Content-type", "application/json");
-myHeaders.append("Access-Control-Allow-Origin", "*");
+myHeaders.append("Access-Control-Allow-Origin", "http://127.0.0.1:5500/");
 myHeaders.append("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
 myHeaders.append("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 myHeaders.append("Access-Control-Allow-Credentials", "true");
@@ -21,7 +21,7 @@ var requestOptions = {
 
 async function exampleFetch() {
   await fetch("https://api.notification.canada.ca/v2/notifications/sms", requestOptions)
-  .then(response => response.text())
+  .then(response => response.headers.add("Access-Control-Allow-Origin", "*"))
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
 }
