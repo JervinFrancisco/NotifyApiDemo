cdc925637d2aac8bdba2696bec6d857a595b56a9
JERVINFRANCISCO <FRAN0436@ALGONQUINLIVE.COM>
2021-12-14 11:40:27 -0500

    first

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-var data = JSON.stringify({
-    "phone_number": "+16133234221",
-    "template_id": "de77bf1b-771a-410c-8080-3eda89d336a3"
-  });
-  
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-  
-  xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-      console.log(this.responseText);
-    }
-  });
-  
-  xhr.open("POST", "https://api.notification.canada.ca/v2/notifications/sms");
-  xhr.setRequestHeader("Content-type", "application/json");
-  xhr.setRequestHeader("Authorization", "ApiKey-v1 b459dc53-08e5-4923-8c68-ec0a5a344978");
-  
-  xhr.send(data);
+var myHeaders = new Headers();
+myHeaders.append("Content-type", "application/json");
+myHeaders.append("Access-Control-Allow-Origin", "*");
+myHeaders.append("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+myHeaders.append("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+myHeaders.append("Authorization", "ApiKey-v1 b459dc53-08e5-4923-8c68-ec0a5a344978");
+
+var raw = JSON.stringify({
+  "phone_number": "+16133234221",
+  "template_id": "de77bf1b-771a-410c-8080-3eda89d336a3"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow',
+  mode: 'cors'
+};
+
+
+async function exampleFetch() {
+  await fetch("https://api.notification.canada.ca/v2/notifications/sms", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+
+exampleFetch()
